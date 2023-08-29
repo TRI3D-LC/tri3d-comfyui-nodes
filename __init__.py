4c9a505d7879cc6ba2361b0e684bc764330ec596
Apple <apple@Apples-MacBook-Pro.local>
2023-08-29 18:38:28 +0530

    adding two images

--- __init__.py
+++ __init__.py
@@ -9,13 +9,57 @@ class Example:
                 "seg" : ("IMAGE",),
             },
         }
-    RETURN_TYPES = ("IMAGE","IMAGE")
+    RETURN_TYPES = ("IMAGE",)
     FUNCTION = "test"
     CATEGORY = "ALPHA"
     def test(self, image,seg):
         import cv2
         import numpy as np
         import torch
+
+        
+        def bounded_image(img,color_code_list, canny_img):
+            import cv2
+            import numpy as np
+            mask = np.zeros_like(canny_img)
+
+            for color_code in color_code_list:
+
+                # Generate a mask for the current color code
+                lower = np.array(color_code, dtype = "uint8")
+                upper = np.array(color_code, dtype = "uint8")
+                color_mask = cv2.inRange(img, lower, upper)
+                # cv2.imwrite(output_dir + str(color_code) + ".jpg",color_mask)
+                # Find contours in the mask
+                contours, _ = cv2.findContours(color_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                
+                for cnt in contours:
+
+                    # x, y, w, h = cv2.boundingRect(cnt)
+
+                    # # Display the region within the bounding box on the mask
+                    # mask[y:y+h, x:x+w] = 255
+
+                    # # print("contour found",cnt)
+                    # Get the convex hull
+                    hull = cv2.convexHull(cnt)
+                    # Draw the convex hull onto the mask
+                    cv2.drawContours(mask, [hull], 0, (255), thickness=cv2.FILLED)
+
+            # cv2.imwrite(output_dir + "mask_original.jpg",mask)
+            # Add a margin around the hand
+            kernel = np.ones((10,10),np.uint8)  # Kernel size determines the size of the dilation (margin size)
+            mask = cv2.dilate(mask, kernel, iterations = 1)
+
+            # Apply the mask to the canny image
+
+            # cv2.imwrite(output_dir + "mask.jpg",mask)
+            bounded_canny_img = cv2.bitwise_and(canny_img, mask)
+            # print(bounded_canny_img.shape)
+            #convert to gray scale
+            bounded_canny_img = cv2.cvtColor(bounded_canny_img, cv2.COLOR_BGR2GRAY)
+            return bounded_canny_img
+        
         def tensor_to_cv2_img(tensor, remove_alpha=False):
             i = 255. * tensor.squeeze(0).cpu().numpy()  # This will give us (H, W, C)
             img = np.clip(i, 0, 255).astype(np.uint8)
@@ -29,10 +73,13 @@ class Example:
         cv2_image = tensor_to_cv2_img(image)    
         cv2_seg = tensor_to_cv2_img(seg)
 
-        result_tensor_img = cv2_img_to_tensor(cv2_image)
-        result_tensor_seg = cv2_img_to_tensor(cv2_seg)
+        # 128 128 64 / 128 128 192
+        color_code_list = [[128,128,64], [128,128,192]]
+        bimage = bounded_image(cv2_seg,color_code_list,cv2_image)
+
+        b_tensor_img = cv2_img_to_tensor(bimage)
         
-        return (result_tensor_img,result_tensor_seg,)
+        return (b_tensor_img,)
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
