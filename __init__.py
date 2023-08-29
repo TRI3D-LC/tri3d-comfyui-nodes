32b09d5e30ef285607d96d2bf6879e5ccc444639
Apple <apple@Apples-MacBook-Pro.local>
2023-08-29 17:06:00 +0530

    adding two images

--- __init__.py
+++ __init__.py
@@ -24,23 +24,42 @@ class Example:
         import numpy as np
         import torch
 
+       
+
+        def print_rgb_histogram(tensor):
+            # Convert tensor to numpy array
+            np_arr = tensor.cpu().numpy()
+
+            # Ensure the tensor has the shape [B, H, W, C] or [H, W, C]
+            if len(np_arr.shape) == 4:
+                np_arr = np_arr[0]
+
+            # Convert the array to a list of tuples (R, G, B)
+            pixels = [tuple(pixel) for row in np_arr for pixel in row]
+
+            # Get the most common RGB pixel values and their counts
+            counter = Counter(pixels)
+            common_pixels = counter.most_common(5)
+
+            print("RGB Pixel Value : Frequency")
+            for pixel, count in common_pixels:
+                print(f"{pixel} : {count}")
+
+                def tensor_to_cv2_img(tensor, remove_alpha=False):
+                    np_img = tensor.cpu().numpy()
+                    if len(np_img.shape) == 3:
+                        np_img = np.transpose(np_img, (1, 2, 0))
+                    np_img = (np_img * 255).astype(np.uint8)
+
+                    # Check for alpha channel in image and optionally remove it
+                    if remove_alpha and np_img.shape[-1] == 4:
+                        np_img = np_img[:, :, :3]
+
+                    return np_img
         print("image", image.shape)
         print("seg", seg.shape)
-
-        
-
-        def tensor_to_cv2_img(tensor, remove_alpha=False):
-            np_img = tensor.cpu().numpy()
-            if len(np_img.shape) == 3:
-                np_img = np.transpose(np_img, (1, 2, 0))
-            np_img = (np_img * 255).astype(np.uint8)
-
-            # Check for alpha channel in image and optionally remove it
-            if remove_alpha and np_img.shape[-1] == 4:
-                np_img = np_img[:, :, :3]
-
-            return np_img
-
+        print_rgb_histogram(image)
+        print_rgb_histogram(seg)
         def cv2_img_to_tensor(cv2_img):
             # Convert image values to [0, 1]
             cv2_img = cv2_img.astype(np.float32) / 255.0
