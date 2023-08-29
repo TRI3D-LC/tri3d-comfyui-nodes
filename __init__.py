fe246e07fbbfb16b2f31dea01793b7af9675706d
Apple <apple@Apples-MacBook-Pro.local>
2023-08-29 18:10:59 +0530

    adding two images

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 class Example:
     def __init__(self):
         pass    
-
     @classmethod
     def INPUT_TYPES(s):
         return {
@@ -16,15 +15,14 @@ class Example:
         import cv2
         import numpy as np
         import torch
-        
         def tensor_to_cv2_img(tensor, remove_alpha=False):
-            i = 255. * tensor.squeeze(0).permute(1, 2, 0).cpu().numpy()  # Changed the format to (H, W, C)
+            i = 255. * tensor.cpu().numpy()
             img = cv2.cvtColor(np.clip(i, 0, 255).astype(np.uint8), cv2.COLOR_BGR2RGB)
             return img
         
         def cv2_img_to_tensor(img):
             img = img.astype(np.float32) / 255.0
-            img = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0)  # Changed the format to (B, C, H, W)
+            img = torch.from_numpy(img)[None,]
             return img
 
         print("image", image.shape, type(image))
@@ -33,6 +31,15 @@ class Example:
         result_tensor_img = cv2_img_to_tensor(cv2_image)
         print("result_tensor_img", result_tensor_img.shape, type(result_tensor_img))
         
-        return result_tensor_img
+        return (result_tensor_img,)
 
-# Your other code remains the same.
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "tri3d-extract-hand": Example
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "tri3d-extract-hand": "Extract Hand"
+}
