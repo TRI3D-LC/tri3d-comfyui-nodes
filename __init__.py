00e3ff971eedf991097bc6c049ce12b9981258d5
Apple <apple@Apples-MacBook-Pro.local>
2023-08-29 18:17:43 +0530

    adding two images

--- __init__.py
+++ __init__.py
@@ -17,10 +17,9 @@ class Example:
         import torch
         def tensor_to_cv2_img(tensor, remove_alpha=False):
             i = 255. * tensor.squeeze(0).cpu().numpy()  # This will give us (H, W, C)
-            img = cv2.cvtColor(np.clip(i, 0, 255).astype(np.uint8), cv2.COLOR_RGB2BGR)  # Change from RGB to BGR
+            img = cv2.cvtColor(np.clip(i, 0, 255).astype(np.uint8))  # Change from RGB to BGR
             return img
 
-        
         def cv2_img_to_tensor(img):
             img = img.astype(np.float32) / 255.0
             img = torch.from_numpy(img)[None,]
