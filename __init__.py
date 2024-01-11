82030c7992cf46e1198ef3c8be96981467d4f132
Apple <apple@Apples-MacBook-Pro.local>
2024-01-11 19:18:26 +0530

    recolor node; 1.8

--- __init__.py
+++ __init__.py
@@ -1549,6 +1549,134 @@ class FloatToImage:
         return image
 
 
+class TRI3D_recolor:
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image_reference": ("IMAGE", ),
+                "image_mask_reference": ("IMAGE", ),
+                "image_recolor": ("IMAGE", ),
+                "image_mask_recolor": ("IMAGE", ),
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE", )
+    FUNCTION = "recolor"
+    CATEGORY = "TRI3D"
+
+    def recolor(self, image_reference, image_mask_reference, image_recolor,
+                image_mask_recolor):
+
+        def get_mu_sigma(array_input, mask_input):
+
+            import numpy as np
+            import math
+
+            array_input = array_input.astype(dtype=np.float32).flatten()
+            mask_input = mask_input.flatten()
+
+            sum = np.sum(mask_input)
+            mean = np.sum(array_input * mask_input) / sum
+
+            array_input -= mean
+            array_input *= mask_input
+            sigma = math.sqrt(np.sum(np.square(array_input)) / sum)
+
+            return mean, sigma
+
+        def do_recolor(image_1, mask_1, image_2, mask_2):
+
+            import cv2
+            import numpy as np
+            import math
+
+            image_2_original = image_2.copy()
+
+            mask_1 = (mask_1 > 127).astype(dtype=np.uint8)
+            mask_2 = (mask_2 > 127).astype(dtype=np.uint8)
+
+            sum_1 = np.sum(mask_1.flatten())
+            sum_2 = np.sum(mask_2.flatten())
+
+            for i in range(3):
+                image_1[:, :, i] *= mask_1
+                image_2[:, :, i] *= mask_2
+
+            image_1 = cv2.cvtColor(image_1, cv2.COLOR_BGR2HSV_FULL)
+            image_2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2HSV_FULL)
+
+            image_1 = image_1.astype(dtype=np.float32)
+            image_2 = image_2.astype(dtype=np.float32)
+
+            for i in range(3):
+
+                mu_1, sigma_1 = get_mu_sigma(array_input=image_1[:, :, i],
+                                             mask_input=mask_1)
+
+                mu_2, sigma_2 = get_mu_sigma(array_input=image_2[:, :, i],
+                                             mask_input=mask_2)
+
+                image_2[:, :, i] = ((
+                    (image_2[:, :, i] - mu_2) / sigma_2) * sigma_1) + mu_1
+
+                image_2[:, :, i] *= mask_2
+
+            image_2 = np.clip(image_2, 0, 255)
+            image_2 = image_2.astype(dtype=np.uint8)
+            image_2 = cv2.cvtColor(image_2, cv2.COLOR_HSV2BGR_FULL)
+
+            for i in range(3):
+
+                image_2_original[:, :,
+                                 i] = (image_2_original[:, :, i] *
+                                       (1 - mask_2)) + (image_2[:, :, i] *
+                                                        mask_2)
+
+            return image_2_original
+
+        def from_torch_image(image):
+
+            image = image.squeeze().cpu().numpy() * 255.0
+            image = np.clip(image, 0, 255).astype(np.uint8)
+
+            return image
+
+        def to_torch_image(image):
+
+            import numpy as np
+            import torch
+            image = image.astype(dtype=np.float32)
+            image /= 255.0
+            image = torch.from_numpy(image)[
+                None,
+            ]
+            image = image.unsqueeze(0)
+            return image
+
+        image_reference = from_torch_image(image=image_reference)
+        image_mask_reference = from_torch_image(image=image_mask_reference)[:, :, 0]
+        image_recolor = from_torch_image(image=image_recolor)
+        image_mask_recolor = from_torch_image(image=image_mask_recolor)[:, :, 0]
+
+        # print('#### DEBUG #### ', image_reference.shape)
+        # print('#### DEBUG #### ', image_mask_reference.shape)
+        # print('#### DEBUG #### ', image_recolor.shape)
+        # print('#### DEBUG #### ', image_mask_recolor.shape)
+
+        image_output = do_recolor(image_1=image_reference,
+                                  mask_1=image_mask_reference,
+                                  image_2=image_recolor,
+                                  mask_2=image_mask_recolor)
+
+        # print('#### DEBUG #### ', image_output.shape)
+
+        image_output = to_torch_image(image=image_output)
+
+        return image_output
+
+
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
@@ -1565,13 +1693,14 @@ NODE_CLASS_MAPPINGS = {
     "tri3d-load-pose-json": TRI3DLoadPoseJson,
     "tri3d-face-recognise": TRI3DFaceRecognise,
     "tri3d-float-to-image": FloatToImage,
+    "tri3d-recolor": TRI3D_recolor,
 
 }
 
 
 
 
-VERSION = "1.7.2"
+VERSION = "1.8.0"
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "tri3d-atr-parse-batch": "ATR Parse Batch" + " v" + VERSION,
@@ -1589,4 +1718,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "tri3d-load-pose-json": "Load Pose Json" + " v" + VERSION,
     "tri3d-face-recognise": "Recognise face" + " v" + VERSION,
     "tri3d-float-to-image": "Render float" + " v" + VERSION,
+    "tri3d-recolor": "Recolor" + " v" + VERSION,
 }
