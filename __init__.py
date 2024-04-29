3a592a5376f76243c22e582da03ff43d8a701845
Apple <apple@Apples-MacBook-Pro.local>
2024-04-29 12:32:30 +0530

    v3.1

--- __init__.py
+++ __init__.py
@@ -2839,7 +2839,7 @@ NODE_CLASS_MAPPINGS = {
     "tri3d-get_histogram_limits": get_histogram_limits,
 }
 
-VERSION = "3.0"
+VERSION = "3.1"
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "tri3d-atr-parse-batch": "ATR Parse Batch" + " v" + VERSION,
