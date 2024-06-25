43815bf7c6e791b88603e5d6b476b682820bbeb7
NitishTRI3D <nitish@tri3d.in>
2024-06-25 07:05:37 +0000

    v3.8 with mega bgremove node

--- __init__.py
+++ __init__.py
@@ -3500,6 +3500,169 @@ class main_transparent_background():
         return (image, mask)
 
 
+
+class TRI3D_BGREMOVE_MEGA():
+    def from_torch_image(self, image):
+        image = image.cpu().numpy() * 255.0
+        image = np.clip(image, 0, 255).astype(np.uint8)
+        return image
+
+
+    def to_torch_image(self, image):
+        image = image.astype(dtype=np.float32)
+        image /= 255.0
+        image = torch.from_numpy(image)
+        return image
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE", ),
+                "method" : ("STRING", {"default": "transparent_background"})
+            },
+        }
+
+    FUNCTION = "run"
+    RETURN_TYPES = ("IMAGE", "MASK")
+    CATEGORY = "TRI3D"
+
+    def run(self, image, method):
+
+        import http.client
+        import mimetypes
+        import os
+        import uuid
+        import dotenv
+
+        dotenv.load_dotenv()
+
+        # Read the API key from the environment variable
+        PHOTOROOM_API_KEY = os.getenv('PHOTOROOM_API_KEY','EMPTY')
+        picked_method = method 
+
+        if method not in ['transparent_background', 'photoroom']:
+            raise ValueError(f"Invalid method {method}")
+
+
+        if PHOTOROOM_API_KEY == 'EMPTY':
+            picked_method = 'transparent_background'
+        
+
+        if picked_method == 'transparent_background':
+            image = self.from_torch_image(image)
+            image = get_transparent_background(image)
+            mask = image[:, :, :, 3]
+            image = image[:, :, :, 0:3]
+            image = self.to_torch_image(image)
+            mask = self.to_torch_image(mask)
+            print('DEBUG', image.shape, mask.shape)
+            return (image, mask)
+        
+        elif picked_method == 'photoroom':
+            print("Using PhotoRoom API")         
+            def tensor_to_cv2_img(tensor, remove_alpha=False):
+                i = 255. * tensor.cpu().numpy()  # This will give us (H, W, C)
+                img = np.clip(i, 0, 255).astype(np.uint8)
+                return img
+
+            def cv2_img_to_tensor(img):
+                img = img.astype(np.float32) / 255.0
+                img = torch.from_numpy(img)[
+                    None,
+                ]
+                return img
+            
+
+            # Please replace with your own apiKey
+
+            def remove_background(input_image_path, output_image_path,apiKey):
+                # Define multipart boundary
+                boundary = '----------{}'.format(uuid.uuid4().hex)
+
+                # Get mimetype of image
+                content_type, _ = mimetypes.guess_type(input_image_path)
+                if content_type is None:
+                    content_type = 'application/octet-stream'  # Default type if guessing fails
+
+                # Prepare the POST data
+                with open(input_image_path, 'rb') as f:
+                    image_data = f.read()
+                filename = os.path.basename(input_image_path)
+
+                body = (
+                f"--{boundary}\r\n"
+                f"Content-Disposition: form-data; name=\"image_file\"; filename=\"{filename}\"\r\n"
+                f"Content-Type: {content_type}\r\n\r\n"
+                ).encode('utf-8') + image_data + f"\r\n--{boundary}--\r\n".encode('utf-8')
+                
+                # Set up the HTTP connection and headers
+                conn = http.client.HTTPSConnection('sdk.photoroom.com')
+
+                headers = {
+                    'Content-Type': f'multipart/form-data; boundary={boundary}',
+                    'x-api-key': apiKey
+                }
+
+                # Make the POST request
+                conn.request('POST', '/v1/segment', body=body, headers=headers)
+                response = conn.getresponse()
+
+                # Handle the response
+                if response.status == 200:
+                    response_data = response.read()
+                    with open(output_image_path, 'wb') as out_f:
+                        out_f.write(response_data)
+                    print("Image saved to", output_image_path)
+                else:
+                    print(f"Error: {response.status} - {response.reason}")
+                    print(response.read())
+
+                # Close the connection
+                conn.close()
+
+                    
+
+            OUTPUT_FOLDER = "output/"
+            batch_results = []
+            batch_results_masks = []
+            images = image
+            for i in range(images.shape[0]):
+                image = images[i]
+                cv2_image = tensor_to_cv2_img(image)
+                cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB)
+                import random 
+                random_number = random.randint(0, 100000)
+                output_path = OUTPUT_FOLDER + f"output{i}_{random_number}.png"
+                input_path = OUTPUT_FOLDER + f"input{i}_{random_number}.png"
+                cv2.imwrite(input_path, cv2_image)
+                remove_background(input_path, output_path, PHOTOROOM_API_KEY)
+
+                print(input_path, output_path)
+                cv2_segm = cv2.imread(output_path, cv2.IMREAD_UNCHANGED)
+                cv2_segm = cv2.cvtColor(cv2_segm, cv2.COLOR_BGRA2RGBA)
+
+                # mask = image[:, :, :, 3]
+                # image = image[:, :, :, 0:3]
+                cv2_segm_mask = cv2_segm[:,:,3]
+                b_tensor_img = cv2_img_to_tensor(cv2_segm)
+                b_tensor_img_mask = cv2_img_to_tensor(cv2_segm_mask)
+                batch_results.append(b_tensor_img.squeeze(0))
+                batch_results_masks.append(b_tensor_img_mask.squeeze(0))
+
+            batch_results = torch.stack(batch_results)
+            batch_results_masks = torch.stack(batch_results_masks)
+
+            return (batch_results,batch_results_masks)
+                
+
+
+
+
+
 from photoroom import TRI3D_photoroom_bgremove_api
 
 # A dictionary that contains all nodes you want to export with their names
@@ -3550,6 +3713,7 @@ NODE_CLASS_MAPPINGS = {
     "tri3d-run_MVANet_inference": run_MVANet_inference,
     'tri3d-load_AEMatter_Model': load_AEMatter_Model,
     'tri3d-run_AEMatter_inference': run_AEMatter_inference,
+    "tri3d-bgremove-mega" :TRI3D_BGREMOVE_MEGA,
 }
 
 VERSION = "3.8"
@@ -3602,4 +3766,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "tri3d-run_MVANet_inference": 'Run MVANet inference' + ' v' + VERSION,
     'tri3d-load_AEMatter_Model': 'Load AEMatter Model' + ' v' + VERSION,
     'tri3d-run_AEMatter_inference': 'Run AEMatter inference' + ' v' + VERSION,
+    "tri3d-bgremove-mega": "BG Remove Mega" + " v" + VERSION,
 }
