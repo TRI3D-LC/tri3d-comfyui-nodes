59aa80b723d85a9e11277deecce100965ff85b1e
aravindhv10 <aravindhv10@gmail.com>
2024-06-28 16:41:07 +0530

    Temporary ugly hack for a100

--- segment_face.py
+++ segment_face.py
@@ -64,10 +64,123 @@ def full_work_wrapper(image):
 
     return tup
 
-    res, n_classes = detect_face_from_tensor(image)
-    tup = do_recolor(res, n_classes)
-    # tup = torch.from_numpy(tup).to(device=image.device, dtype=image.dtype)
+
+def run_slave(input_image_path, output_image_path, tmp_file_path):
+    import os
+
+    EXEC_STRING = '''
+import torch
+import facer
+import cv2
+import einops
+import numpy as np
+import sys
+
+
+def load_image(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(image, code=cv2.COLOR_BGR2RGB)
+    image = torch.from_numpy(image).to(dtype=torch.float32) / 255.0
+    return image
+
+
+def do_recolor(vis_seg_probs, n_classes):
+    val = int(255 / n_classes)
+    vis_seg_probs = vis_seg_probs.cpu().detach().numpy()
+    not_visible = (vis_seg_probs == 0).astype(dtype=np.uint8)
+    not_visible = 1 - not_visible
+    not_visible *= 255
+    vis_seg_probs *= val
+    ret = np.array((vis_seg_probs, not_visible, not_visible), np.uint8)
+    ret = einops.rearrange(ret, 'c h w -> h w c')
+    ret = cv2.cvtColor(ret, cv2.COLOR_HSV2BGR_FULL)
+    return ret
+
+
+def detect_face_from_tensor(image):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    image *= 255
+    image = image.to(dtype=torch.uint8)
+    image = facer.hwc2bchw(image).to(device=device)
+    face_detector = facer.face_detector('retinaface/mobilenet', device=device)
+
+    with torch.inference_mode():
+        faces = face_detector(image)
+
+    face_parser = facer.face_parser(
+        'farl/lapa/448', device=device)  # optional "farl/celebm/448"
+
+    with torch.inference_mode():
+        faces = face_parser(image, faces)
+
+    seg_logits = faces['seg']['logits']
+    seg_probs = seg_logits.softmax(dim=1)  # nfaces x nclasses x h x w
+    n_classes = seg_probs.size(1)
+
+    vis_seg_probs = seg_probs.argmax(dim=1)
+    vis_seg_probs = einops.einsum(vis_seg_probs, 'b h w -> h w')
+    return (vis_seg_probs, n_classes)
+
+
+def full_work_wrapper(image):
+    try:
+        res, n_classes = detect_face_from_tensor(image)
+        tup = do_recolor(res, n_classes)
+    except:
+        print('Warning: Failed to find a face.')
+        tup = np.zeros(image.shape, dtype=np.uint8)
     return tup
+
+tup = full_work_wrapper(image=load_image(image_path=sys.argv[1]))
+cv2.imwrite(sys.argv[2], tup)
+'''
+
+    with open(tmp_file_path, 'w', encoding='utf-8') as f:
+        f.write(EXEC_STRING)
+
+    CMD = 'python3 ' + tmp_file_path + ' ' + input_image_path + ' ' + output_image_path
+
+    print(CMD)
+    os.system(CMD)
+
+
+def run_slave_tensor(image):
+    import tempfile
+    import cv2
+    import os
+
+    device = image.device
+    outtype = image.dtype
+
+    path_dir = tempfile.TemporaryDirectory(
+        suffix='.dir',
+        prefix='facer.',
+        dir=None,
+        ignore_cleanup_errors=False,
+    )
+
+    path_input = path_dir.name + '/input.png'
+    path_output = path_dir.name + '/output.png'
+    path_source = path_dir.name + '/exec.py'
+
+    image = image.detach().cpu().numpy() * 255.0
+    image = image.astype(dtype=np.uint8)
+    image = cv2.cvtColor(src=image, code=cv2.COLOR_RGB2BGR)
+    cv2.imwrite(path_input, image)
+
+    run_slave(input_image_path=path_input,
+              output_image_path=path_output,
+              tmp_file_path=path_source)
+
+    os.unlink(path_input)
+    os.unlink(path_source)
+    image = cv2.imread(path_output, cv2.IMREAD_COLOR)
+    os.unlink(path_output)
+    os.rmdir(path_dir.name)
+    # image = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2RGB)
+    image = image.astype(np.float32) / 255.0
+    # image = torch.from_numpy(image).to(dtype=outtype, device=device) / 255.0
+    return image
 
 
 class main_face_segment():
@@ -91,7 +204,8 @@ class main_face_segment():
         batch_size = image.shape[0]
         ret = []
         for i in range(batch_size):
-            ret.append(full_work_wrapper(image[i].clone()))
+            # ret.append(full_work_wrapper(image[i].clone()))
+            ret.append(run_slave_tensor(image[i].clone()))
 
         ret = np.array(ret)
         ret = torch.from_numpy(ret).to(dtype=image.dtype, device=image.device)
