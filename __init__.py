9e601095c4a36ad5b9cf0dc732759cdb1fceb6ed
Ubuntu <ubuntu@ResearchA105.am5vh0lh1w1ujfpmyrlqrv1c0c.bx.internal.cloudapp.net>
2024-07-01 09:50:27 +0000

    v4.2; optimised facer node

--- __init__.py
+++ __init__.py
@@ -3726,7 +3726,7 @@ NODE_CLASS_MAPPINGS = {
 }
 
 
-VERSION = "4.1"
+VERSION = "4.2"
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "tri3d-photoroom-bgremove-api": "Photoroom BG Remove" + " v" + VERSION,
