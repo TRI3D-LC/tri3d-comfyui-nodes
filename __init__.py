cf47490051a135972b95218a4c4d8eb748ce5079
Ubuntu <ubuntu@Loras1US1.h15swyaai0nejdtueailbmozue.bx.internal.cloudapp.net>
2025-03-10 05:57:46 +0000

    neck_modify 4.9.0v

--- __init__.py
+++ __init__.py
@@ -3766,7 +3766,7 @@ NODE_CLASS_MAPPINGS = {
 }
 
 
-VERSION = "4.8.7"
+VERSION = "4.9.0"
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "tri3d-photoroom-bgremove-api": "Photoroom BG Remove" + " v" + VERSION,
