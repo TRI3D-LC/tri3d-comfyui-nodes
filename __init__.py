2a4e6731d2e894480e64c231dcddd212d635e7ec
Ubuntu <ubuntu@ip-172-31-0-186.ec2.internal>
2025-05-27 12:08:19 +0000

    version 5.0.0

--- __init__.py
+++ __init__.py
@@ -3776,7 +3776,7 @@ NODE_CLASS_MAPPINGS = {
 }
 
 
-VERSION = "4.9.0"
+VERSION = "5.0.0"
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "tri3d-photoroom-bgremove-api": "Photoroom BG Remove" + " v" + VERSION,
